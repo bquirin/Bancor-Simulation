187e7ac6744d83f298fbac340b42d0be7a5a1136
bquirin <brandon.quirin@gmail.com>
2019-04-18 20:46:32 +0100

    Reformat code

--- BancorFormulas.py
+++ BancorFormulas.py
@@ -1,6 +1,7 @@
-# Bancor Formulas
+# Bancor Formulas #
 
-#Calculate price given R,S and F
+
+# Calculate price given R,S and F given reserve balance, supply and CRR
 def calcPrice(reserve_balance, supply, CRR):
     price = reserve_balance / (supply * CRR)
     return price
@@ -12,31 +13,32 @@ def calcTokensIssued(supply, tokens_given, reserve_balance, CRR):
     return tokens_issued
 
 
-#Calculates Reserve tokens issued E in Exchange for smart tokens paid T given R,S and F
+# Calculates Reserve tokens issued E in Exchange for smart tokens paid T
+# given R,S and F
 def calcReserveIssued(supply, tokens_given, reserve_balance, CRR):
-    reserve_issued = reserve_balance*(1.0 - (1.0 - tokens_given/supply)**(1.0/CRR))
+    reserve_issued = reserve_balance*(1.0-(1.0-tokens_given/supply)**(1.0/CRR))
     return reserve_issued
 
 
-#Calculates Effective Price to account for price slippage
+# Calculates Effective Price to account for price slippage
 def calcEffectivePrice(tokens_given, tokens_issued):
     effective_price = tokens_given / tokens_issued
     return effective_price
 
 
-#Calculates the conversion rate for of Token1/Token2
+# Calculates the conversion rate for of Token1/Token2
 def calcReserveRate(reserve_balance1, reserve_balance2):
-    price = reserve_balance1/ reserve_balance2
+    price = reserve_balance1 / reserve_balance2
     return price
 
 
-#Increase the balance by the amount given
+# Increase the balance by the amount given
 def increaseBalance(balance, amount):
     balance = balance + amount
     return balance
 
 
-#Decrease the balance by the amount given
+# Decrease the balance by the amount given
 def decreaseBalance(balance, amount):
     balance = balance - amount
     return balance
